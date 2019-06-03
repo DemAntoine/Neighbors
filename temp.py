7f08a962ee9c2c8b464983a1c7a81ea5634285f1
EC2 Default User <ec2-user@ip-172-31-45-20.us-east-2.compute.internal>
2019-06-03 13:28:47 +0000

    upd db

--- temp.py
+++ temp.py
@@ -3,32 +3,46 @@ from peewee import datetime as peewee_datetime
 from models import User
 from openpyxl import load_workbook
 
-wb_base = load_workbook(filename='workbook1.xlsx', read_only=False, keep_vba=True)
-ws_data = wb_base['sheet2']
+# wb_base = load_workbook(filename='workbook1.xlsx', read_only=False, keep_vba=True)
+# ws_data = wb_base['sheet2']
 
 
-for i in range(2, ws_data.max_row):
+# for i in range(2, ws_data.max_row):
     
-    User.create(
+#     User.create(
     
-        user_id = ws_data.cell(row=i, column=8).value,
-        username = ws_data.cell(row=i, column=5).value,
-        first_name = ws_data.cell(row=i, column=6).value,
-        last_name = ws_data.cell(row=i, column=7).value,
+#         user_id = ws_data.cell(row=i, column=8).value,
+#         username = ws_data.cell(row=i, column=5).value,
+#         first_name = ws_data.cell(row=i, column=6).value,
+#         last_name = ws_data.cell(row=i, column=7).value,
     
-        house = 2,
-        section = ws_data.cell(row=i, column=4).value,
-        floor = ws_data.cell(row=i, column=3).value,
-        apartment = ws_data.cell(row=i, column=9).value,
-    )
+#         house = 2,
+#         section = ws_data.cell(row=i, column=4).value,
+#         floor = ws_data.cell(row=i, column=3).value,
+#         apartment = ws_data.cell(row=i, column=9).value,
+#     )
 
 
 # query = User.select().where(User.id > 10)
-# # query = User.delete().where(User.id > 10)
-# # query.execute()
+# query = User.delete().where(User.user_id == 274285845)
+# query.execute()
 
 # for i in query:
 #     print(i.id)
 
 # # print(peewee_datetime.datetime.now())
 # # print(peewee_datetime.datetime.today().day)
+
+
+User.create(
+
+    user_id = 752042597,
+    # username = ws_data.cell(row=i, column=5).value,
+    first_name = 'Iryna',
+    # last_name = 'Костенко',
+
+    house = 2,
+    section = 1,
+    floor = 9,
+    # apartment = ws_data.cell(row=i, column=9).value,
+)
