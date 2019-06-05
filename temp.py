2cbba5f437d248da76d3b9182db1011536196885
EC2 Default User <ec2-user@ip-172-31-45-20.us-east-2.compute.internal>
2019-06-05 06:44:51 +0000

    version 1

--- temp.py
+++ temp.py
@@ -1,22 +1,21 @@
 from peewee import SqliteDatabase, IntegerField, Model, DateTimeField, CharField, ForeignKeyField
 from peewee import datetime as peewee_datetime
 from models import User
-# from openpyxl import load_workbook
+from openpyxl import load_workbook
 
 # wb_base = load_workbook(filename='workbook1.xlsx', read_only=False, keep_vba=True)
 # ws_data = wb_base['sheet2']
 
 # ws_data.max_row
 
-# users = User.select()
-# for i in range(19, 52):
+# # users = User.select()
+# # for i in range(19, 52):
     
 # User.create(
-
-#     user_id = 848451586,
-#     # username = 'demydenko',
-#     first_name = 'А2',
-#     # last_name = 'Демиденко',
+#     user_id = ,
+#     username = '',
+#     first_name = '',
+#     last_name = '',
 
 #     house = 4,
 #     section = 4,
@@ -26,25 +25,25 @@ from models import User
 
 
 # query = User.select().where(User.id > 10)
-query = User.delete().where(User.user_id == 848451586)
-query.execute()
+# query = User.delete().where(User.user_id == )
+# query.execute()
 
 # for i in query:
 #     print(i.id)
 
-# # print(peewee_datetime.datetime.now())
-# # print(peewee_datetime.datetime.today().day)
+# print(peewee_datetime.datetime.now())
+# print(peewee_datetime.datetime.today().day)
 
 
 # User.create(
 
-#     user_id = 752042597,
-#     # username = ws_data.cell(row=i, column=5).value,
-#     first_name = 'Iryna',
-#     # last_name = 'Костенко',
+#     user_id = ,
+#     username = ws_data.cell(row=i, column=5).value,
+#     first_name = '',
+#     last_name = '',
 
 #     house = 2,
 #     section = 1,
 #     floor = 9,
-#     # apartment = ws_data.cell(row=i, column=9).value,
+#     apartment = ws_data.cell(row=i, column=9).value,
 # )
