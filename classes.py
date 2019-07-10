30efd02432754f40b72e35d86e8fc4680783f33b
EC2 Default User <ec2-user@ip-172-31-45-20.us-east-2.compute.internal>
2019-07-10 08:36:00 +0000

    fix integer filter

--- classes.py
+++ classes.py
@@ -3,16 +3,16 @@ import re
 
 
 class MyFilters(BaseFilter):
-    """Custom text filters
-    """
+    """Custom text filters"""
 
     def call_err(self, message):
         return 'error' in message.text
 
     def integers(self, message):
-        pattern = r'^[0-9]+$'
-        found = re.findall(pattern, message.text)
-        return found if found else False
+        if message.text:
+            pattern = r'^[0-9]+$'
+            found = re.findall(pattern, message.text)
+            return found if found else False
 
     def flood(self, message):
         pattern = r'\bфлуд\w{,4}\b|\bбубнил\w{,4}\b'
@@ -31,16 +31,3 @@ filt_integers = MyFilters().integers
 filt_call_err = MyFilters().call_err
 filt_flood = MyFilters().flood
 filt_fuck = MyFilters().fuck
-
-
-# pattern = r'(флуд\w{,4}\s{,2}|бубнилк\w{,4}\s)'
-# pattern = r'\bфлуд\w{,4}\b|\bбубнилк\w{,4}\b'
-# string = 'here is a mpythonm флудhggg         string for бубнилк Pythontt things'
-# re.findall(pattern=pattern, string=string, flags=re.IGNORECASE)
-#
-# print(True if re.findall(pattern, string, flags=re.IGNORECASE) else False)
-#
-# print(re.findall(pattern, string, flags=re.IGNORECASE))
-
-
-# print(True == [False, []])
