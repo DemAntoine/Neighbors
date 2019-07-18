94f4ad523931c1020e68e2157a1b7db79de4e049
EC2 Default User <ec2-user@ip-172-31-45-20.us-east-2.compute.internal>
2019-07-18 07:14:23 +0000

    improve pattern

--- classes.py
+++ classes.py
@@ -24,13 +24,6 @@ class MyFilters(BaseFilter):
             return found if found else False
 
     @staticmethod
-    def fuck(message):
-        pattern = r'\bхуй\w{,4}\b|\bп.зда\w{,4}\b'
-        if message.text:
-            found = re.findall(pattern, message.text, flags=re.IGNORECASE)
-            return found if found else False
-
-    @staticmethod
     # block forwarding msg from specific users
     def open_data_ua_bot(message):
         if message.forward_from:
@@ -40,13 +33,12 @@ class MyFilters(BaseFilter):
 filt_integers = MyFilters().integers
 filt_call_err = MyFilters().call_err
 filt_flood = MyFilters().flood
-filt_fuck = MyFilters().fuck
 # filt_open_data_ua_bot = MyFilters().open_data_ua_bot
 
 
 class FilterBlock(BaseFilter):
     def filter(self, message):
-        pattern = r'\bхуй\w{,4}\b|\bп.зда\w{,4}\b'
+        pattern = r'\bху[е,й,я]\w{,4}\b|\bп[і,ы,и]зда\w{,4}\b'
         if message.text:
             found = re.findall(pattern, message.text, flags=re.IGNORECASE)
             return found if found else False
